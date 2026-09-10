999b9ae5f8aad7a0c33362401767c681d9a0b835
VEDANT KHAMBADKAR <136165426+Vedant-khambadkar@users.noreply.github.com>
2026-09-10 12:41:13 +0530

    feat: replace package archive with App component importing BookDemo page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import BookDemo from './pages/BookDemo';
-import HomeScreen from './pages/HomeScreen';
 
 export default function App() {
   return (
     <>
-      <HomeScreen />
+      <BookDemo />
     </>
 
   )
